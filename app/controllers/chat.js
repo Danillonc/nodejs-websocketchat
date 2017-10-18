87b1d884c441b113c973e4961479cd4dc37d6f54
danillonc <danillonc@live.com>
2017-10-18 17:45:44 -0200

    errors form validation

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciaChat = function(application, req, res){
+
+	var dadosForm = req.body;
+
+	req.assert('apelido', 'Nome/Apelido é obrigatório.').notEmpty();
+	req.assert('apelido', 'Nome/Apelido deve conter entre 3 e 15 caracteres.').len(3, 10);
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render('index', {validacao : erros});
+		return;
+	}
+
 	res.render('chat');
 }
