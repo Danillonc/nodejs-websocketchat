cc0a348be6f48e62af79fd4dabf3b29e6c9ee0a4
danillonc <danillonc@live.com>
2017-10-19 15:43:06 -0200

    implementing emit socket function in server side and on function client side to exchange msgs

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-var app = require('./config/server');
+var app = require('./config/server'); //importing config server file.
 
-app.listen(80, function(){
+var server = app.listen(80, function(){ //open connection on port 80.
     console.log('Servidor ON');
 });
+
+var io = require('socket.io').listen(server); //importing socket.io module.
+
+app.set('io', io); //setting global variable using express.
+
+//start conn using socket.
+io.on('connection', function(){
+	console.log('Usuario conectou');
+
+	io.on('disconnect', function(){
+		console.log('Usuario desconectou.');
+	})
+}); 
