b63796db8e51239e1201ea05682a9353b35849e0
danillonc <danillonc@live.com>
2017-10-19 17:29:27 -0200

    update chat list

--- app.js
+++ app.js
@@ -9,10 +9,29 @@ var io = require('socket.io').listen(server); //importing socket.io module.
 app.set('io', io); //setting global variable using express.
 
 //start conn using socket.
-io.on('connection', function(){
+io.on('connection', function(socket){
 	console.log('Usuario conectou');
 
-	io.on('disconnect', function(){
+	socket.on('disconnect', function(){
 		console.log('Usuario desconectou.');
-	})
+	});
+
+	socket.on('msgParaServidor', function(data){
+
+		  /* emit event to client about dialog messages */
+          socket.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
+
+          socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
+
+          /* emit evento to client about chat list */
+          if(parseInt(data.apelido_atualizado) == 0){
+
+            socket.emit('participantesParaCliente', {apelido: data.apelido});
+
+            socket.broadcast.emit('participantesParaCliente', {apelido: data.apelido});
+          
+          }
+
+          	
+	});
 }); 
